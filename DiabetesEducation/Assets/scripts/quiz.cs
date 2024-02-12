581e8f2428ade644ddcee33069ee3c2234749b5b
Khopylium <juvaill@etud.univ-angers.fr>
2024-02-12 18:37:46 +0100

    Quiz durations changed

--- DiabetesEducation/Assets/scripts/quiz.cs
+++ DiabetesEducation/Assets/scripts/quiz.cs
@@ -3,9 +3,11 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
 using System.Collections;
+using TMPro.Examples;
 
 public class quiz : MonoBehaviour
 {
+    public static bool success_quiz;
     public TMP_Text questionText;
     public Button[] answerButtons;
     
@@ -13,8 +15,10 @@ public class quiz : MonoBehaviour
     private Question currentQuestion;
     private int questionNumber;    
     private int score, totalScore;
+    private int answerDuration = 5;
     void Start()
     {
+        success_quiz = false;
         score = 0;
         totalScore = 0;
         InitQuestions();
@@ -29,19 +33,20 @@ public class quiz : MonoBehaviour
 
     void InitQuestions()
     {
+        const int nb_reponses = 4;
         questions = new List<Question>
         {
-            new("Quel diabète touche le plus de personne ?", new string[4] { "Diabète de type I", "Diabète de type II", null, null }, 1),
-            new("Où je jette les aiguilles des seringues à insuline ?", new string[4] { "Dans une poubelle spéciale", "A la poubelle normale", null, null }, 0),
-            new("Quel facteur ne participe PAS à l’apparition du diabète de type 2 ?", new string[4] { "L’hérédité", "Une mauvaise alimentation", "Le stress", "Un manque d’activité physique" }, 0),
+            new("Quel diabète touche le plus de personne ?", new string[nb_reponses] { "Diabète de type I", "Diabète de type II", null, null }, 1),
+            new("Où je jette les aiguilles des seringues à insuline ?", new string[nb_reponses] { "Dans une poubelle spéciale", "A la poubelle normale", null, null }, 0),
+            new("Quel facteur ne participe PAS à l’apparition du diabète de type 2 ?", new string[nb_reponses] { "L’hérédité", "Une mauvaise alimentation", "Le stress", "Un manque d’activité physique" }, 0),
             //new("Qui est la meilleure waifu ?", new string[4] { "Anime girl 1", "Anime girl 2", "Anime girl 3", "Nicolas Cage" }, 3),  //A ENLEVER
-            new("Combien existe-t-il de diabète?", new string[4] { "1", "2", "3", null }, 1),
-            new("Le diabète est-il douloureux ?", new string[4] { "Oui", "Non", null, null }, 1),
-            new("Comment s'appelle le taux de sucre que l'on a dans le sang ?", new string[4]{"La glycémie", "La sucrémie", "L’insulinémie", null}, 0),
-            new("Ai-je le droit de pratiquer du sport en tant que diabétique ?", new string[4]{"Oui", "Non", null, null}, 0),
-            new("Est-ce que je dois totalement arrêter de manger du sucre si je suis diabétique ?", new string[4]{"Oui", "Non", null, null}, 1),
-            new("Comment mesurer ma glycémie ?", new string[4]{"A l’aide d’un glucomètre", "A l’aide d’un test urinaire", null, null}, 1),
-            new("Le diabète m’empêche de vivre une vie normale ?", new string[4]{"Oui", "Non", null, null}, 1)
+            new("Combien existe-t-il de diabète?", new string[nb_reponses] { "1", "2", "3", null }, 1),
+            new("Le diabète est-il douloureux ?", new string[nb_reponses] { "Oui", "Non", null, null }, 1),
+            new("Comment s'appelle le taux de sucre que l'on a dans le sang ?", new string[nb_reponses]{"La glycémie", "La sucrémie", "L’insulinémie", null}, 0),
+            new("Ai-je le droit de pratiquer du sport en tant que diabétique ?", new string[nb_reponses]{"Oui", "Non", null, null}, 0),
+            new("Est-ce que je dois totalement arrêter de manger du sucre si je suis diabétique ?", new string[nb_reponses]{"Oui", "Non", null, null}, 1),
+            new("Comment mesurer ma glycémie ?", new string[nb_reponses]{"A l’aide d’un glucomètre", "A l’aide d’un test urinaire", null, null}, 1),
+            new("Le diabète m’empêche de vivre une vie normale ?", new string[nb_reponses]{"Oui", "Non", null, null}, 1)
         };
         
     }
@@ -64,6 +69,7 @@ public class quiz : MonoBehaviour
             answerButtons[i].gameObject.SetActive(false);
        }
        questionText.text = "Quiz terminé! \nTon score final est de "+score+" sur " + totalScore; 
+        success_quiz = true;
    }
 
     public void CheckAnswer(int buttonIndex)
@@ -88,7 +94,7 @@ public class quiz : MonoBehaviour
         Image buttonImage = button.GetComponent<Image>();
         Color originalColor = buttonImage.color;
         buttonImage.color = Color.green;
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(answerDuration);
         buttonImage.color = originalColor;
 
         if(questions.Count > questionNumber+1){
@@ -104,7 +110,7 @@ public class quiz : MonoBehaviour
         Image buttonImage = button.GetComponent<Image>();
         Color originalColor = buttonImage.color;
         buttonImage.color = Color.red;
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(answerDuration);
         buttonImage.color = originalColor;
     }
 }
